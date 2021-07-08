60ae7ca17bdeeacad94c87fbe3ad61a53c7db690
Jodi Ivan Lumbantoruan <jodiivanz@gmail.com>
2021-07-09 01:15:19 +0700

    conservatiive way part 1

--- adapter/try_svg.go
+++ adapter/try_svg.go
@@ -9,11 +9,17 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-const lowecaseLength = 30
+const lowecaseLength = 15
 const upperCaseLegth = 30
 
+type NoteTies struct {
+	StartIndex int
+	EndIndex   int
+}
 type NoteGoup struct {
-	Notes []Note
+	Notes  []Note
+	Ties   []NoteTies
+	Dotted []int
 }
 type Note struct {
 	Numbered int64
@@ -32,6 +38,21 @@ type Row struct {
 	Lyrics     map[int][]Lyric
 }
 
+type Position struct {
+	X int
+	Y int
+}
+
+type GroupPosition struct {
+	Start Position
+	End   Position
+}
+
+type RowGroupLocator struct {
+	NoteGroupLoc GroupPosition
+	LyricLoc     GroupPosition
+}
+
 type TrySVG struct {
 }
 
@@ -44,6 +65,31 @@ func (ts *TrySVG) ServeHTTP(w http.ResponseWriter, r *http.Request, ps httproute
 					Note{
 						Numbered: 3,
 					},
+					Note{
+						Numbered: 6,
+					},
+					Note{
+						Numbered: 7,
+					},
+					Note{
+						Numbered: 3,
+					},
+					Note{
+						Numbered: 6,
+					},
+					Note{
+						Numbered: 7,
+					},
+				},
+				Ties: []NoteTies{
+					NoteTies{
+						StartIndex: 0,
+						EndIndex:   4,
+					},
+					NoteTies{
+						StartIndex: 0,
+						EndIndex:   5,
+					},
 				},
 			},
 			NoteGoup{
@@ -53,6 +99,18 @@ func (ts *TrySVG) ServeHTTP(w http.ResponseWriter, r *http.Request, ps httproute
 					},
 					Note{
 						Numbered: 6,
+					},
+					Note{
+						Numbered: 7,
+					},
+					Note{
+						Numbered: 6,
+					},
+				},
+				Ties: []NoteTies{
+					NoteTies{
+						StartIndex: 0,
+						EndIndex:   1,
 					},
 				},
 			},
@@ -101,47 +159,127 @@ func (ts *TrySVG) ServeHTTP(w http.ResponseWriter, r *http.Request, ps httproute
 	w.WriteHeader(200)
 	s := svg.New(w)
 	s.Start(500, 500)
-	// Text(x int, y int, t string, s ...string)
+
+	verse := 1
+
+	lengthLyric := make([]int64, len(aRow.NoteGroups))
+	lengthNote := make([]int64, len(aRow.NoteGroups))
 
 	for i, d := range aRow.NoteGroups {
-		if i == 0 {
-			if len(d.Notes) == 1 {
-				s.Text(100, 100, fmt.Sprint(d.Notes[0].Numbered))
-			} else {
-				for j, g := range d.Notes {
-					s.Text(100+(lowecaseLength*j), 100, fmt.Sprint(g.Numbered))
-				}
-			}
-		} else {
-			if len(d.Notes) == 1 {
-				s.Text(100+(lowecaseLength*i*len(aRow.Lyrics[1][i].Text)), 100, fmt.Sprintf(" %d", d.Notes[0].Numbered))
-			} else {
-				for j, g := range d.Notes {
-					if j == 0 {
-						text := fmt.Sprint(g.Numbered)
-						if j == 0 {
-							text = fmt.Sprintf(" %s", text)
-						}
-						s.Text(100+((j+1)*(lowecaseLength*i*len(aRow.Lyrics[1][i].Text))), 100, text)
-					} else {
-						s.Text(115+(j*(lowecaseLength*i*len(aRow.Lyrics[1][i].Text))), 100, fmt.Sprint(g.Numbered))
+		currLyricLength := len(aRow.Lyrics[verse][i].Text)
+		lengthLyric[i] = int64(currLyricLength)
 
-					}
-				}
+		currNoteLength := len(d.Notes) + len(d.Dotted)
+		if len(d.Notes) > 1 {
+			// count the space
+			currNoteLength = currNoteLength + (len(d.Notes) - 1)
+		}
+
+		if len(d.Dotted) > 1 {
+			// count the space
+			currNoteLength = currNoteLength + (len(d.Dotted) - 1)
+		}
+		lengthNote[i] = int64(currNoteLength)
+
+		fmt.Println("Note", i, "lyric", currLyricLength, "notes", currNoteLength)
+	}
+
+	initialX := 100
+	initialY := 100
+	offset := 0
+
+	for i, d := range aRow.NoteGroups {
+		initialX = initialX + offset
+
+		if len(d.Notes) == 1 {
+			s.Text(initialX, initialY, fmt.Sprint(d.Notes[0].Numbered))
+			initialX += 9
+		} else {
+			locX := map[int]int{}
+			for j, g := range d.Notes {
+				s.Text(initialX+(lowecaseLength*j), initialY, fmt.Sprint(g.Numbered))
+				locX[j] = initialX + (lowecaseLength * j)
 			}
+			for i, tie := range d.Ties {
+				//                                                             M1                       M1                C1                          C2
+				path := fmt.Sprintf("M%f,%d,C%f,%d %f,%d %f,%d", float64(locX[tie.StartIndex])+2.5, initialY+5, float64(locX[tie.StartIndex])+2.5, initialY+10+(i*5), float64(locX[tie.EndIndex])+3.5, initialY+10, float64(locX[tie.EndIndex])+3.5, initialY+5)
+				s.Path(path, `fill-opacity="0"`, `stroke="#000"`, `stroke-width="1"`)
+			}
+		}
+
+		offset = int(lengthNote[i]) * 9
+		if offset < int(lengthLyric[i]) {
+			offset = int(lengthLyric[i]) * 9
 		}
 	}
 
-	for i, d := range aRow.Lyrics[1] {
-		text := d.Text
-		if !d.IsEndword {
-			text = fmt.Sprintf("%s -", text)
-		}
-		if i == 0 {
-			s.Text(100, 120, text)
-		} else {
-			s.Text(100+(lowecaseLength*i*len(aRow.Lyrics[1][i].Text)), 120, text)
-		}
-	}
+	s.Path("M 10 10 C 20 20, 40 20, 50 10")
+	// } else {
+	// 	locX := map[int]int{}
+	// 	for j, g := range d.Notes {
+	// 		// s.Text(100+(lowecaseLength*j), 100, fmt.Sprint(g.Numbered))
+	// 		locX[j] = 100 + (lowecaseLength * j)
+	// 	}
+	// 	for _, tie := range d.Ties {
+	// 		path := fmt.Sprintf("M%f,%d,C%f,%d %f,%d %f,%d", float64(locX[tie.StartIndex])+2.5, 105, float64(locX[tie.StartIndex])+2.5, 110, float64(locX[tie.EndIndex])+3.5, 110, float64(locX[tie.EndIndex])+3.5, 105)
+	// 		s.Path(path, `fill-opacity="0"`, `stroke="#000"`, `stroke-width="1"`)
+	// 	}
+	// }
+
+	// Text(x int, y int, t string, s ...string)
+
+	// for i, d := range aRow.NoteGroups {
+	// 	if i == 0 {
+	// 		if len(d.Notes) == 1 {
+	// 			s.Text(100, 100, fmt.Sprint(d.Notes[0].Numbered))
+	// 		} else {
+	// 			locX := map[int]int{}
+	// 			for j, g := range d.Notes {
+	// 				s.Text(100+(lowecaseLength*j), 100, fmt.Sprint(g.Numbered))
+	// 				locX[j] = 100 + (lowecaseLength * j)
+	// 			}
+	// 			// for _, tie := range d.Ties {
+	// 			// 	// path := fmt.Sprintf("M100,200,C100,300 400,300 400,200")
+	// 			// 	path := fmt.Sprintf("M%f,%d,C%f,%d %f,%d %f,%d", float64(locX[tie.StartIndex])+2.5, 105, float64(locX[tie.StartIndex])+2.5, 110, float64(locX[tie.EndIndex])+3.5, 110, float64(locX[tie.EndIndex])+3.5, 105)
+	// 			// 	s.Path(path, `fill-opacity="0"`, `stroke="#000"`, `stroke-width="1"`)
+	// 			// }
+	// 		}
+	// 	} else {
+	// 		if len(d.Notes) == 1 {
+	// 			s.Text(100+(lowecaseLength*i*len(aRow.Lyrics[1][i].Text)), 100, fmt.Sprintf(" %d", d.Notes[0].Numbered))
+	// 		} else {
+	// 			// locX := map[int]int{}
+	// 			for j, g := range d.Notes {
+	// 				if j == 0 {
+	// 					text := fmt.Sprint(g.Numbered)
+	// 					if j == 0 {
+	// 						text = fmt.Sprintf(" %s", text)
+	// 					}
+	// 					s.Text(100+(lowecaseLength*i), 100, text)
+	// 				} else {
+	// 					s.Text(115+(j*(lowecaseLength*i)), 100, fmt.Sprint(g.Numbered))
+	// 				}
+	// 			}
+
+	// 			// for _, tie := range d.Ties {
+	// 			// 	// path := fmt.Sprintf("M100,200,C100,300 400,300 400,200")
+	// 			// 	path := fmt.Sprintf("M%f,%d,C%f,%d %f,%d %f,%d", float64(locX[tie.StartIndex])+2.5, 105, float64(locX[tie.StartIndex])+2.5, 110, float64(locX[tie.EndIndex])+3.5, 110, float64(locX[tie.EndIndex])+3.5, 105)
+	// 			// 	s.Path(path, `fill-opacity="0"`, `stroke="#000"`, `stroke-width="1"`)
+	// 			// }
+	// 		}
+	// 	}
+	// }
+
+	// for i, d := range aRow.Lyrics[1] {
+	// 	text := d.Text
+	// 	if !d.IsEndword {
+	// 		text = fmt.Sprintf("%s -", text)
+	// 	}
+	// 	if i == 0 {
+	// 		s.Text(100, 125, text)
+	// 	} else {
+	// 		s.Text(100+(lowecaseLength*i*len(aRow.Lyrics[1][i].Text)), 125, text)
+	// 	}
+	// }
 	s.End()
 }

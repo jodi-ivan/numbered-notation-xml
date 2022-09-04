f21eca611ee74f9964777bf3f85db7d6471a5dc8
Jody Ivan Lumbantoruan <jodi.lumbantoruan@tokopedia.com>
2022-09-05 01:20:12 +0700

    WIP(movable do): list the scale of the major and minor

--- internal/renderer/numbered.go
+++ internal/renderer/numbered.go
@@ -8,6 +8,42 @@ import (
 	"github.com/jodi-ivan/numbered-notation-xml/internal/musicxml"
 )
 
+var majorScale = map[string][]string{
+	"cis": []string{"C#", "D#", "E#", "F#", "G#", "A#", "B#"},
+	"fis": []string{"F#", "G#", "A#", "B", "C#", "D#", "E#"},
+	"b":   []string{"B", "C#", "D#", "E", "F#", "G#", "A#"},
+	"e":   []string{"E", "F#", "G#", "A", "B", "C#", "D#"},
+	"a":   []string{"A", "B", "C#", "D", "E", "F#", "G#"},
+	"d":   []string{"D", "E", "F#", "G", "A", "B", "C#"},
+	"g":   []string{"G", "A", "B", "C", "D", "E", "F#"},
+	"c":   []string{"C", "D", "E", "F", "G", "A", "B"},
+	"f":   []string{"F", "G", "A", "Bb", "C", "D", "E"},
+	"bes": []string{"Bb", "C", "D", "Eb", "F", "G", "A"},
+	"es":  []string{"Eb", "F", "G", "Ab", "Bb", "C", "D"},
+	"as":  []string{"Ab", "Bb", "C", "Db", "Eb", "F", "G"},
+	"des": []string{"Db", "Eb", "F", "Gb", "Ab", "Bb", "C"},
+	"ges": []string{"Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"},
+	"ces": []string{"Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"},
+}
+
+var minorScale = map[string][]string{
+	"ais": []string{"A#", "B#", "C#", "D#", "E#", "F#", "G#"},
+	"dis": []string{},
+	"gis": []string{},
+	"cis": []string{},
+	"fis": []string{},
+	"b":   []string{},
+	"e":   []string{},
+	"a":   []string{},
+	"d":   []string{},
+	"g":   []string{},
+	"c":   []string{},
+	"f":   []string{},
+	"bis": []string{},
+	"es":  []string{},
+	"as":  []string{},
+}
+
 const LOWERCASE_LENGTH = 15
 const LAYOUT_INDENT_LENGTH = 50
 
@@ -26,15 +62,15 @@ var majorKeySignature = map[int]string{
 	-4: "as",  // Ab
 	-5: "des", // Db
 	-6: "ges", //Gb
-	-7: "cis", //Cb
+	-7: "ces", //Cb
 }
 
 var minorKeySignature = map[int]string{
-	7:  "ais",
-	6:  "dis",
-	5:  "gis",
-	4:  "cis",
-	3:  "fis",
+	7:  "ais", // A#
+	6:  "dis", // D#
+	5:  "gis", // G#
+	4:  "cis", // C#
+	3:  "fis", // F#
 	2:  "b",
 	1:  "e",
 	0:  "a",
@@ -42,9 +78,9 @@ var minorKeySignature = map[int]string{
 	-2: "g",
 	-3: "c",
 	-4: "f",
-	-5: "bis",
-	-6: "es",
-	-7: "as",
+	-5: "bis", //F#
+	-6: "es",  //Eb
+	-7: "as",  // Ab
 }
 
 func RenderNumbered(w http.ResponseWriter, music musicxml.MusicXML) {
@@ -73,7 +109,16 @@ func RenderNumbered(w http.ResponseWriter, music musicxml.MusicXML) {
 	s.Text(LAYOUT_INDENT_LENGTH, relativeY, keySignature)
 
 	// render time signature
+	// TODO:
+	// - check the time signature on github issue
 	beat := music.Part.Measures[0].Attribute.Time
 	s.Text(LAYOUT_INDENT_LENGTH+(len(keySignature)*LOWERCASE_LENGTH), relativeY, fmt.Sprintf("%d ketuk", beat.Beats))
+
 	s.End()
+}
+
+func RenderMeasure(s *svg.SVG, measure musicxml.Measure, attributes musicxml.Attribute) {
+
+	// map the key signature
+
 }

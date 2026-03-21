4c4da930c0e1bc4c2a68feff3cb6133bed8a6725
Jody Ivan Lumbantoruan <jodiivanz@gmail.com>
2026-03-21 10:35:04 +0700

    feat(search): dedicated ngram field

--- cmd/lab/search/main.go
+++ cmd/lab/search/main.go
@@ -10,7 +10,13 @@ import (
 	"strings"
 	"unicode"
 
+	_ "github.com/blevesearch/bleve/v2/analysis/analyzer/custom"
+	_ "github.com/blevesearch/bleve/v2/analysis/token/lowercase"
+	_ "github.com/blevesearch/bleve/v2/analysis/token/ngram"
+	_ "github.com/blevesearch/bleve/v2/analysis/tokenizer/unicode"
+
 	"github.com/blevesearch/bleve/v2"
+	"github.com/blevesearch/bleve/v2/analysis/token/lowercase"
 	"github.com/jodi-ivan/numbered-notation-xml/cmd/lab/search/usage"
 	"github.com/jodi-ivan/numbered-notation-xml/internal/lyric"
 	"github.com/jodi-ivan/numbered-notation-xml/internal/musicxml"
@@ -24,6 +30,11 @@ func init() {
 	if originalTitleExp == nil {
 		originalTitleExp = regexp.MustCompile("<i>.*</i>")
 	}
+
+	// registry.RegisterTokenFilter("ngram", func(config map[string]interface{}, cache *registry.Cache) (analysis.TokenFilter, error) {
+	// 	return ngram.NgramFilterConstructor(config, cache)
+	// })
+
 }
 
 type VerseDoc struct {
@@ -46,95 +57,26 @@ type Document struct {
 	ForKids       bool          `json:"for_kids"`
 	Copyright     string        `json:"copyright,omitempty"`
 	MusicCredit   string        `json:"music"`
-}
-
-var categories = map[string]map[string][2]int{
-	"Menghadap Allah": map[string][2]int{
-		"Puji-pujian dan Pembukaan Ibadah": [2]int{1, 22},
-		"Pengakuan dan Pengampunan Dosa":   [2]int{23, 41},
-		"Kyrie dan Gloria":                 [2]int{42, 48},
-	},
-	"Pelayanan Firman": map[string][2]int{
-		"Pembacaan Alkitab":                    [2]int{49, 59},
-		"Penciptaan dan Pemeliharaan":          [2]int{60, 69},
-		"Pejanjian Lama":                       [2]int{70, 75},
-		"Penantian Mesias dan Masa Adven":      [2]int{76, 91},
-		"Kelahiran Yesus dan Masa Natal":       [2]int{92, 127},
-		"Akhir Masa Natal dan Epifania":        [2]int{128, 143},
-		"Kisah Pelayanan Yesus":                [2]int{144, 154},
-		"Masa Prapaskah":                       [2]int{155, 163},
-		"Sengsara Yesus dan Jumat Agung":       [2]int{164, 186},
-		"Kebangkitan Yesus dan Masa Paskah":    [2]int{187, 217},
-		"Hari Kenaikan":                        [2]int{218, 227},
-		"Roh Kudus dan Hari Pentakosta":        [2]int{228, 241},
-		"Allah Tritunggal dan Hari Trinitatis": [2]int{242, 246},
-		"Gereja dan Kerajaan Allah":            [2]int{247, 261},
-		"Kehidupan Sorgawi":                    [2]int{262, 271},
-		"Akhir Zaman dan Penggenapan":          [2]int{272, 279},
-	},
-	"Respons Terhadap Pelayanan Firman": map[string][2]int{
-		"Pernyataan Keyakinan Iman":         [2]int{280, 285},
-		"Pengucapan Syukur dan Persembahan": [2]int{286, 303},
-	},
-	"Pelayanan Khusus": map[string][2]int{
-		"Baptisan Kudus dan Peneguhan Sidi": [2]int{304, 309},
-		"Perjamuan Kudus":                   [2]int{310, 315},
-		"Pernikahan":                        [2]int{316, 318},
-		"Peristiwa Isimewa Gerejawi":        [2]int{319, 320},
-	},
-	"Waktu dan Musim": map[string][2]int{
-		"Pagi dan Siang":    [2]int{321, 323},
-		"Petang dan Malam":  [2]int{323, 329},
-		"Pergantian Tahun":  [2]int{330, 332},
-		"Musim dan Panen":   [2]int{333, 335},
-		"Bangsa dan Negara": [2]int{336, 337},
-	},
-	"Penutupan Ibadah": map[string][2]int{
-		"Pungutusan": [2]int{338, 344},
-		"Berkat":     [2]int{345, 350},
-	},
-	"Hidup Beriman Sehari-hari": map[string][2]int{
-		"Panggilan Juruselamat":           [2]int{351, 360},
-		"Penyerahan Diri":                 [2]int{361, 376},
-		"Kebesaran Rahmat Tuhan":          [2]int{377, 390},
-		"Sukacita dalam Tuhan":            [2]int{391, 399},
-		"Hidup Bersama Tuhan":             [2]int{400, 405},
-		"Tuntunan Tuhan":                  [2]int{406, 421},
-		"Tanggung Jawab Pengikut Kristus": [2]int{422, 437},
-		"Kemenangan dalam Perjuangan":     [2]int{438, 446},
-		"Keluarga dan Persekutuan":        [2]int{447, 451},
-		"Doa dan Setiap Waktu":            [2]int{452, 471},
-	},
-	"Haleluya, Amin dan Lain-lain": map[string][2]int{
-		"": [2]int{472, 478},
-	},
-}
-
-func GetCategory(num int) (string, string) {
-
-	for cat, subs := range categories {
-		for subcat, ranges := range subs {
-			if num >= ranges[0] && num <= ranges[1] {
-				return cat, subcat
-			}
-		}
-	}
-
-	return "", ""
-
+	Title_ngram   string        `json:"title_ngram"`
 }
 
 func GetOriginalTitle(metadata *repository.HymnMetadata) []string {
-	if strings.Contains(metadata.Lyric, "<i>") {
-		submatch := originalTitleExp.FindStringSubmatch(metadata.Lyric)
-		if len(submatch) > 0 && len(submatch[0]) > 6 {
-			stripped := submatch[0][3 : len(submatch[0])-4]
-			strings.TrimSuffix(stripped, ",")
-			return strings.Split(stripped, "/")
-		}
+	result := []string{}
+	if strings.HasPrefix(metadata.Lyric, "<i>") {
+		index := strings.Index(metadata.Lyric, "</i>")
+		lyric := metadata.Lyric[3:index]
+		lyric = strings.TrimSpace(lyric)
+		lyric = strings.TrimSuffix(lyric, ",")
+		result = strings.Split(lyric, "/")
+	} else {
+		result = []string{metadata.Title}
 	}
 
-	return []string{metadata.Title}
+	for i, v := range result {
+		result[i] = strings.TrimSpace(v)
+	}
+
+	return result
 }
 
 func BuildContent(repo repository.Repository, metadata *repository.HymnMetadata, verse int) string {
@@ -242,6 +184,7 @@ func BuildDocument(repo repository.Repository, num int, vaiant ...string) (*Docu
 	doc := &Document{
 		ID:            strconv.Itoa(metadata.HymnID),
 		Title:         metadata.Title,
+		Title_ngram:   metadata.Title,
 		HymnNo:        metadata.Number,
 		BE:            int(metadata.RefBE.Int16),
 		NR:            int(metadata.RefNR.Int16),
@@ -305,6 +248,30 @@ func main() {
 	}
 	defer index.Close()
 
+	// 1. Create a custom token filter for n-grams (3 to 6 characters is usually sweet spot)
+	err = indexMapping.AddCustomTokenFilter("my_ngram",
+		map[string]interface{}{
+			"type": "ngram",
+			"min":  3,
+			"max":  6,
+		})
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 2. Create an analyzer using that filter
+	err = indexMapping.AddCustomAnalyzer("part_word_analyzer",
+		map[string]interface{}{
+			"type":      "custom",
+			"tokenizer": "unicode",
+			"token_filters": []string{
+				lowercase.Name,
+				"my_ngram",
+			},
+		})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	docMapping := bleve.NewDocumentMapping()
 
 	contentMapping := bleve.NewTextFieldMapping()
@@ -332,8 +299,15 @@ func main() {
 	hymnMap := bleve.NewNumericFieldMapping()
 	hymnMap.Store = true
 
-	titleMap := bleve.NewNumericFieldMapping()
+	titleMap := bleve.NewTextFieldMapping()
 	titleMap.Store = true
+
+	titleNgram := bleve.NewTextFieldMapping()
+	titleNgram.Store = true
+	titleNgram.Analyzer = "part_word_analyzer"
+
+	ogMap := bleve.NewTextFieldMapping()
+	ogMap.Store = true
 
 	versesMapping.AddFieldMappingsAt("no", noMapping)
 	versesMapping.AddFieldMappingsAt("content", verseContentMapping)
@@ -343,6 +317,8 @@ func main() {
 	docMapping.AddFieldMappingsAt("variant", variantMapping)
 	docMapping.AddFieldMappingsAt("hymn_no", hymnMap)
 	docMapping.AddFieldMappingsAt("title", titleMap)
+	docMapping.AddFieldMappingsAt("original_title", ogMap)
+	docMapping.AddFieldMappingsAt("title_ngram", titleNgram)
 
 	indexMapping.DefaultMapping = docMapping
 
@@ -361,7 +337,7 @@ func main() {
 	// Add documents
 	documents := []*Document{}
 
-	for no := 1; no <= 25; no++ {
+	for no := 1; no <= 214; no++ {
 		var doc *Document
 		var err error
 		if variant, ok := variants[no]; ok {
@@ -392,7 +368,6 @@ func main() {
 	batch := index.NewBatch()
 	for _, doc := range documents {
 		batch.Index(doc.ID, doc)
-		log.Println("indexing", doc.ID)
 	}
 	if err := index.Batch(batch); err != nil {
 		log.Fatal(err)
--- cmd/lab/search/query/main.go
+++ cmd/lab/search/query/main.go
@@ -5,9 +5,38 @@ import (
 	"log"
 
 	"github.com/blevesearch/bleve/v2"
+	_ "github.com/blevesearch/bleve/v2/analysis/lang/id"
 )
 
+type VerseDoc struct {
+	No      int    `json:"no"`
+	Content string `json:"content"`
+	Start   int    `json:"start"`
+	End     int    `json:"end"`
+}
+
+func toFloat64Slice(v interface{}) []float64 {
+	switch val := v.(type) {
+	case []interface{}:
+		result := make([]float64, len(val))
+		for i, item := range val {
+			result[i] = item.(float64)
+		}
+		return result
+	case float64:
+		return []float64{val} // wrap scalar in slice
+	}
+	return nil
+}
+
 func main() {
+
+	// db, err := storage.NewStorage(context.Background(), "files/database/kidung-jemaat.db")
+	// if err != nil {
+	// 	log.Fatalf("Failed to connect to storage: %s", err.Error())
+	// 	return
+	// }
+	// repo := repository.New(context.Background(), db)
 
 	indexPath := "files/index/kj.bleve"
 
@@ -18,53 +47,168 @@ func main() {
 	}
 	defer index.Close()
 
-	// Search the created index
-	query := bleve.NewMatchQuery("sembah")
-	query.Fuzziness = 1
-	query.Prefix = 1
-	searchRequest := bleve.NewSearchRequest(query)
-	searchRequest.Fields = []string{"hymn_no", "title", "verses.no", "verses.content", "verses.start", "verses.end", "variant"}
+	// q := bleve.NewWildcardQuery("*dalam*")
+	// q.SetField("content")
+
+	// q3 := bleve.NewWildcardQuery("*dalam*")
+	// q3.SetBoost(3)
+	// q3.SetField("title")
+
+	// combined1 := bleve.NewDisjunctionQuery(q, q3)
+
+	// searchRequest := bleve.NewSearchRequest(combined1)
+	// searchRequest.IncludeLocations = true
+	// searchRequest.Fields = []string{"hymn_no", "content", "title", "verses.no", "verses.content", "verses.start", "verses.end", "variant", "original_title"}
+	// searchRequest.Highlight = bleve.NewHighlight()
+	// searchRequest.Explain = true
+	// searchResult, err := index.Search(searchRequest)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// log.Println("taken:", searchResult.Took)
+
+	// // result := map[int][]int{}
+	// for _, hit := range searchResult.Hits {
+
+	// 	matchedVerseNos := map[int][]string{}
+
+	// 	if locations, ok := hit.Locations["content"]; ok {
+
+	// 		rawStarts := toFloat64Slice(hit.Fields["verses.start"])
+	// 		rawEnds := toFloat64Slice(hit.Fields["verses.end"])
+	// 		rawNos := toFloat64Slice(hit.Fields["verses.no"])
+	// 		log.Println("Score", hit.Fields["hymn_no"].(float64), ": ", hit.Score)
+	// 		for term, termLocations := range locations {
+	// 			for _, loc := range termLocations {
+	// 				// find which verse this offset falls in
+	// 				for i := range rawStarts {
+	// 					start := rawStarts[i]
+	// 					end := rawEnds[i]
+	// 					if loc.Start >= uint64(start) && loc.Start < uint64(end) {
+	// 						if matchedVerseNos[int(rawNos[i])] == nil {
+	// 							matchedVerseNos[int(rawNos[i])] = []string{}
+	// 						}
+	// 						matchedVerseNos[int(rawNos[i])] = append(matchedVerseNos[int(rawNos[i])], term)
+	// 						break
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 		var verseNos []int
+	// 		for no := range matchedVerseNos {
+	// 			verseNos = append(verseNos, no)
+	// 		}
+	// 		sort.Ints(verseNos)
+
+	// 		for _, no := range verseNos {
+	// 			fmt.Printf("Found: %s. hymn %.0f — matched verses: %v\n", matchedVerseNos[no], hit.Fields["hymn_no"].(float64), no)
+	// 		}
+	// 		fmt.Println("")
+	// 	}
+
+	// 	if locations, ok := hit.Locations["title"]; ok {
+	// 		for term, termLocations := range locations {
+	// 			for _, loc := range termLocations {
+	// 				// find which verse this offset falls in
+	// 				fmt.Printf("[%.0f] Title term %q matched at byte offset %d: %s\n", hit.Fields["hymn_no"].(float64), term, loc.Start, hit.Fields["title"])
+
+	// 			}
+	// 		}
+	// 	}
+
+	// }
+
+	// q1 := bleve.NewWildcardQuery("*dalam*")
+	// q1.SetField("title")
+	// q1.SetBoost(3)
+
+	// q2 := bleve.NewWildcardQuery("*dalam*")
+	// q2.SetField("original_title")
+
+	// combined := bleve.NewDisjunctionQuery(q1, q2)
+
+	// searchRequest1 := bleve.NewSearchRequest(combined)
+	// searchRequest1.IncludeLocations = true
+	// searchRequest1.Fields = []string{"hymn_no", "content", "title", "verses.no", "verses.content", "verses.start", "verses.end", "variant"}
+	// searchRequest1.Highlight = bleve.NewHighlight()
+	// searchRequest1.Explain = true
+	// searchResult1, err := index.Search(searchRequest1)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// log.Println("taken:", searchResult1.Took)
+
+	// // result := map[int][]int{}
+	// for _, hit := range searchResult1.Hits {
+
+	// 	log.Println("===================", hit.Fields["hymn_no"], hit.Fields["variant"])
+
+	// 	log.Println("Score", hit.Fields["hymn_no"].(float64), ": ", hit.Score)
+
+	// 	if locations, ok := hit.Locations["original_title"]; ok {
+	// 		for term, termLocations := range locations {
+	// 			for _, loc := range termLocations {
+	// 				// find which verse this offset falls in
+	// 				fmt.Printf("Original title term %q matched at byte offset %d: %s\n", term, loc.Start, hit.Fields["original_title"])
+	// 			}
+	// 		}
+	// 	}
+
+	// 	if locations, ok := hit.Locations["title"]; ok {
+	// 		for term, termLocations := range locations {
+	// 			for _, loc := range termLocations {
+	// 				// find which verse this offset falls in
+	// 				fmt.Printf("Title term %q matched at byte offset %d: %s\n", term, loc.Start, hit.Fields["title"])
+	// 			}
+	// 		}
+	// 	}
+
+	// 	fmt.Println("")
+
+	// }
+
+	q := bleve.NewPrefixQuery("hai")
+	q.SetField("title_ngram")
+
+	// q1 := bleve.NewMatchQuery("t'lah")
+	// q1.SetField("title_ngram")
+
+	// combined := bleve.NewDisjunctionQuery(q1, q)
+
+	searchRequest := bleve.NewSearchRequest(q)
+	searchRequest.IncludeLocations = true
+	searchRequest.Fields = []string{"hymn_no", "variant", "title", "title_ngram"}
 	searchRequest.Highlight = bleve.NewHighlight()
 	searchRequest.Explain = true
 	searchResult, err := index.Search(searchRequest)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	result := map[int][]int{}
+	log.Println("taken:", searchResult.Took)
+
 	for _, hit := range searchResult.Hits {
 
-		// starts := hit.Fields["verses.start"].([]interface{})
-		// ends := hit.Fields["verses.end"].([]interface{})
+		log.Println("===================", hit.Fields["hymn_no"], hit.Fields["variant"])
 
-		// for i, s := range starts {
-		// 	start := int(s.(float64))
-		// 	end := int(ends[i].(float64))
-		// }
+		log.Println("Score", hit.Fields["hymn_no"].(float64), ": ", hit.Score)
 
-		// hit.Fields contains your stored values
-		// hit.Locations contains the match positions per field
-		log.Println("===================", hit.Fields["hymn_no"], hit.Fields["variant"], "|", hit.Fields["verses.start"], hit.Fields["verses.end"], hit.Fields["verses.no"])
-		// get match positions in "content"
-		if locations, ok := hit.Locations["content"]; ok {
+		if locations, ok := hit.Locations["title_ngram"]; ok {
 			for term, termLocations := range locations {
 				for _, loc := range termLocations {
-
-					fmt.Printf("term %q matched at byte offset %d\n", term, loc.Start)
-					hymnNo := hit.Fields["hymn_no"].(float64)
-					if result[int(hymnNo)] == nil {
-						result[int(hymnNo)] = []int{}
-					}
-
-					result[int(hymnNo)] = append(result[int(hymnNo)], int(loc.Start))
-
-					log.Println("document ID", hit.ID)
-
+					// find which verse this offset falls in
+					fmt.Printf("Original title term %q matched at byte offset %d: %s\n", term, loc.Start, hit.Fields["title_ngram"])
 				}
 			}
 		}
 
-		log.Println(result)
+		fmt.Println("")
+
 	}
 
 }

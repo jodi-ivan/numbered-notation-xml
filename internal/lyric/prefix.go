975d0847b2d700575004e69d64ef3f4490088391
Jody Ivan Lumbantoruan <jodiivanz@gmail.com>
2026-04-08 09:28:21 +0700

    feat(lyric): improve the prefix lyric with splitting it

--- internal/lyric/prefix.go
+++ internal/lyric/prefix.go
@@ -3,8 +3,6 @@ package lyric
 import (
 	"strings"
 
-	"github.com/jodi-ivan/numbered-notation-xml/internal/barline"
-	"github.com/jodi-ivan/numbered-notation-xml/internal/constant"
 	"github.com/jodi-ivan/numbered-notation-xml/internal/entity"
 	"github.com/jodi-ivan/numbered-notation-xml/internal/musicxml"
 )
@@ -23,44 +21,37 @@ func (li *lyricInteractor) CalculateMarginLeft(txt string) float64 {
 
 func (li *lyricInteractor) SplitLyricPrefix(note *entity.NoteRenderer, part int, leftBarline *entity.NoteRenderer) []LyricPosition {
 	lyricVal := entity.LyricVal(note.Lyric[part].Text).String()
-	if note.PositionX == constant.LAYOUT_INDENT_LENGTH || (leftBarline != nil && leftBarline.Barline != nil) {
-		xPos := note.PositionX
 
-		hasLeftBarline := leftBarline != nil && leftBarline.Barline != nil
-		if len(note.Lyric) > 2 {
+	if !numberedLyric.Match([]byte(lyricVal)) {
+		return nil
+	}
+	xPos := float64(note.PositionX)
+	yPos := float64(note.PositionY + 25 + (part * LINE_BETWEEN_LYRIC))
 
-			if hasLeftBarline {
-				xPos -= leftBarline.Width - barline.BARLINE_AFTER_SPACE + 5
-			}
+	parts := strings.Split(lyricVal, ".")
+	if len(parts) == 2 {
 
-			parts := strings.Split(lyricVal, ".")
-			if len(parts) == 2 {
-
-				header := LyricPosition{
-					Coordinate: entity.NewCoordinate(float64(xPos)-li.CalculateLyricWidth(parts[0]+"."), float64(note.PositionY+25+(part*LINE_BETWEEN_LYRIC))),
-					Lyrics: entity.Lyric{
-						Syllabic: musicxml.LyricSyllabicTypeSingle,
-						Text:     []entity.Text{{Value: parts[0] + "."}},
-					},
-				}
-
-				mainLyric := LyricPosition{
-					Coordinate: entity.NewCoordinate(float64(xPos), float64(note.PositionY+25+(part*LINE_BETWEEN_LYRIC))),
-					Lyrics:     note.Lyric[part],
-				}
-				mainLyric.Lyrics.Text[0] = entity.Text{Value: strings.ReplaceAll(mainLyric.Lyrics.Text[0].Value, parts[0]+".", "")}
-				return []LyricPosition{header, mainLyric}
-			}
-		}
-
-		xPos += int(li.CalculateMarginLeft(lyricVal))
-		return []LyricPosition{
-			{
-				Coordinate: entity.NewCoordinate(float64(xPos), float64(note.PositionY+25+(part*LINE_BETWEEN_LYRIC))),
-				Lyrics:     note.Lyric[part],
+		header := LyricPosition{
+			Coordinate: entity.NewCoordinate(xPos-li.CalculateLyricWidth(parts[0]+"."), yPos),
+			Lyrics: entity.Lyric{
+				Syllabic: musicxml.LyricSyllabicTypeSingle,
+				Text:     []entity.Text{{Value: parts[0] + "."}},
 			},
 		}
+
+		mainLyric := LyricPosition{
+			Coordinate: entity.NewCoordinate(xPos, yPos),
+			Lyrics:     note.Lyric[part],
+		}
+		mainLyric.Lyrics.Text[0] = entity.Text{Value: strings.ReplaceAll(mainLyric.Lyrics.Text[0].Value, parts[0]+".", "")}
+		return []LyricPosition{header, mainLyric}
+	}
+	xPos += li.CalculateMarginLeft(lyricVal)
+	return []LyricPosition{
+		{
+			Coordinate: entity.NewCoordinate(xPos, yPos),
+			Lyrics:     note.Lyric[part],
+		},
 	}
 
-	return nil
 }
